264169c0b28bb7b341c6b926245e41782463dfb1
juriiqclash-hash <juriiq.clash@gmail.com>
2026-07-18 12:14:50 +0200

    add postinstall getting-started message

---
+++ bin/postinstall.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+// Runs automatically right after `npm install -g quecksilver-cli` finishes,
+// so people aren't left staring at a blank terminal wondering what to do next.
+
+console.log('');
+console.log('QueckSilver CLI installed!');
+console.log('');
+console.log('Get started:');
+console.log('  quecksilver          Start chatting (will prompt you to log in)');
+console.log('  quecksilver login    Log in or switch accounts');
+console.log('  quecksilver logout   Log out');
+console.log('');
